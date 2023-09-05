a327d3adf215dc9c1b9dc6e7cf59d93bfdb1d306
VinothKumar6595 <vinothcassillas@gmail.com>
2023-09-05 14:33:34 +0530

    Cart

---
+++ src/components/Cart.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import DisabledByDefaultIcon from "@mui/icons-material/DisabledByDefault";
+
+const cartElements = [
+  {
+    title: "Colors",
+    price: 100,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    quantity: 2,
+  },
+  {
+    title: "Black and white Colors",
+    price: 50,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    quantity: 3,
+  },
+  {
+    title: "Yellow and Black Colors",
+    price: 70,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    quantity: 1,
+  },
+];
+
+const Cart = ({ setCartToggle }) => {
+  const [filteredArr, setFilteredArr] = useState(cartElements);
+  const removeListHandler = (title) => {
+    setFilteredArr((prev) =>
+      prev.filter((item) => {
+        return item.title !== title;
+      })
+    );
+  };
+  return (
+    <div className="h-[700px] w-[500px] bg-gray-200 absolute top-16 right-0 ">
+      <div>
+        <div className="absolute right-2">
+          <button onClick={() => setCartToggle(false)}>
+            <DisabledByDefaultIcon />
+          </button>
+        </div>
+        <h1 className="text-2xl font-bold text-center mt-2">CART</h1>
+      </div>
+      <div className="flex justify-between  mt-5 font-bold ">
+        <span className="ml-2 w-64">ITEM</span>
+        <span className="w-12">PRICE</span>
+        <span className="w-36">QUANTITY</span>
+      </div>
+      {filteredArr.map((item) => {
+        return (
+          <div className="flex justify-between mt-5 font-bold ">
+            <span className="flex w-64 flex-wrap">
+              <img src={item.imageUrl} width="50px" className="ml-2 mr-2" />{" "}
+              {item.title}
+            </span>
+            <span className="w-12 text-center">{item.price}</span>
+            <span className="w-36 ml-2">
+              {item.quantity}
+              <button
+                onClick={() => {
+                  removeListHandler(item.title);
+                }}
+                className="bg-red-500 p-1.5 rounded-md ml-2 text-sm"
+              >
+                REMOVE
+              </button>
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import Cart from "./Cart";
 
 const Header = () => {
+  const [cartToggle, setCartToggle] = useState(false);
   return (
     <div>
       <div className="w-full shadow-md flex items-center pl-2">
@@ -10,9 +12,13 @@ const Header = () => {
           <li>Store</li>
           <li>About</li>
         </ul>
-        <button className="mr-10 px-4 py-2 hover:bg-gray-900 hover:text-white rounded-md bg-gray-200 text-black">
+        <button
+          className="mr-10 px-4 py-2 hover:bg-gray-900 hover:text-white rounded-md bg-gray-200 text-black"
+          onClick={() => setCartToggle((prev) => !prev)}
+        >
           Cart
         </button>
+        {cartToggle && <Cart setCartToggle={setCartToggle} />}
       </div>
     </div>
   );
